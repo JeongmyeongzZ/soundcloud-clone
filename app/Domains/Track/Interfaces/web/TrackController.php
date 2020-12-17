86afd799b788f7aa32e91d103b3379904d30af1b
jeongmyeong <kimbym23@gmail.com>
2020-12-17 23:15:18 +0900

    create default domain structure

---
+++ app/Domains/Track/Interfaces/web/TrackController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domains\Track\Interfaces\web;
+
+use App\Http\Controllers\Controller;
+
+class TrackController extends Controller
+{
+
+}
--- app/Domains/Track/Track.php
+++ app/Domains/Track/Track.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Models;
+namespace App\Domains\Track;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
