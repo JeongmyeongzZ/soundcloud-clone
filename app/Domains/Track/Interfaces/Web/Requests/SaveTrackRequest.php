992b6c0a44f6bc7ee8080ffef8bf547fe1d78912
jeongmyeong <kimbym23@gmail.com>
2021-02-18 23:14:10 +0900

    code error commit

--- app/Domains/Track/Interfaces/Web/Requests/SaveTrackRequest.php
+++ app/Domains/Track/Interfaces/Web/Requests/SaveTrackRequest.php
@@ -2,10 +2,14 @@
 
 namespace App\Domains\Track\Interfaces\Web\Requests;
 
+use App\Http\Requests\MapHttpRequest;
 use Illuminate\Foundation\Http\FormRequest;
+use ReflectionException;
 
 class SaveTrackRequest extends FormRequest
 {
+    use MapHttpRequest;
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -17,5 +21,21 @@ class SaveTrackRequest extends FormRequest
             'title' => 'required|string|max:60',
             'artworkUrl' => 'required|string|url',
         ];
+    }
+
+    /**
+     * Map with custom request object class.
+     *
+     * @return mixed|object
+     * @throws ReflectionException
+     */
+    public function toRequestObject()
+    {
+        $this->json()->set('trackId', $this->route()->parameter('trackId'));
+
+        return $this->mapHttpRequestToRequestObject(
+            $this->json(),
+            \App\Domains\Track\Requests\SaveTrackRequest::class
+        );
     }
 }
